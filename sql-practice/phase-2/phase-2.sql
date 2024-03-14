704866f028bbc0b88df15531e98e35a5699156ca
Antonio <antonioramirez9614@gmail.com>
2024-03-14 15:54:25 -0500

    phase 1 and some phase-2 complete

--- sql-practice/phase-2/phase-2.sql
+++ sql-practice/phase-2/phase-2.sql
@@ -1,5 +1,33 @@
+DROP TABLE IF EXISTS  owners;
+DROP TABLE IF EXISTS cats;
+DROP TABLE IF EXISTS cat_owners;
+DROP TABLE IF EXISTS toys;
 PRAGMA foreign_keys=on; -- Turns foreign key support in SQLite3 on
--- Your code here 
+-- Your code here
+
+CREATE TABLE owners (
+  id INT PRIMARY key,
+  first_name varchar(100),
+  last_name varchar(100)
+);
+
+CREATE TABLE cats (
+  id INTEGER PRIMARY key,
+  name VARCHAR(100),
+  birth_year INTEGER,
+  FOREIGN KEY cat_owner REFERENCES cat_owners(cat_id)
+);
+
+CREATE TABLE cat_owners (
+  cat_id INTEGER FOREIGN KEY REFERENCES cats(id),
+  owner_id INTEGER FOREIGN KEY REFERENCES owners(id)
+);
+
+CREATE TABLE toys(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(100),
+  FOREIGN KEY cat_id REFERENCES cats(id)
+  );
 
 INSERT INTO owners
   (first_name, last_name)
